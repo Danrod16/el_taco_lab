ab7922e42a9e6a399295713f75a9c0e81248f2c3
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-02-07 11:42:39 +0100

    FEAT: about route created

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :cases
   devise_for :users
   root to: 'pages#home'
+  get 'about',to: 'pages#about', as: :about
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
